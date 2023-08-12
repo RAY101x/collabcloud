13e80c9654a29191ea2ea8c0b957eed01f0fd741
RAY101x <131998895+RAY101x@users.noreply.github.com>
2023-08-13 01:36:59 +0600

    Navigation update for file

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -14,18 +14,13 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-<<<<<<< Updated upstream
-                    </x-nav-link>
-
-                    <x-nav-link href="{{ route('files') }}" :active="request()->routeIs('files')">
-                        {{ __('Files') }}
-                    </x-nav-link>
-                    
-=======
                     </x-jet-nav-link>
->>>>>>> Stashed changes
+                    <x-jet-nav-link href="{{ route('files') }}" :active="request()->routeIs('files')">
+                        {{ __('Files') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
+
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
